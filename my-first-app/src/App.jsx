bcb9fe8f329fe15c5fe6afe7719af9363795a569
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2023-10-25 23:23:28 +0530

    update

---
+++ my-first-app/src/App.jsx
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home.jsx'
+import Login from './Components/Login.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>home</h1>
+    </div>
+    
+    
+  );
+}
+
+export default App;
