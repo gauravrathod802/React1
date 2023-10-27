f103d5aaeaff7dd730a44fcfff2b1fd28006a3d9
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2023-10-27 11:03:51 +0530

    addToCartButtons

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -1,16 +1,29 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Home from './Components/Home.jsx'
 import Login from './Components/Login.jsx'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>home</h1>
-    </div>
-    
-    
-  );
+    const[count,setCounter]=useState(0);
+
+    function increment(){
+        setCounter((preValue)=>preValue+1);
+    }
+
+    function decrement(){
+        setCounter((preValue)=>preValue-1);
+    }
+
+
+    return(
+        <div>
+        <h1>Cart</h1>
+        <button onClick={increment}>+</button>
+        <span>{count}</span>
+        <button onClick={decrement}>-</button>
+        </div>
+    )
 }
 
 export default App;
--- my-first-app/src/Components/Home.jsx
+++ my-first-app/src/Components/Home.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import './Styles/Home.css'
+import React, { useState } from "react";
+import './Styles/Home.css';
+
 
 const Home = () =>{
-    return(
-        <div>Home</div>
-    )
+return(
+    <div>Home</div>
+);
 }
 
 export default Home;
