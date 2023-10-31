bc62c043f84d2489ce7616767e04d2856b4de21d
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2023-11-01 00:50:35 +0530

    Update Login.jsx

--- my-first-app/src/Components/Login.jsx
+++ my-first-app/src/Components/Login.jsx
@@ -1,6 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 function Login(){
+    const redirect=useNavigate();
+
+    function gotoRegister(){
+        redirect('/Register');
+    }
 return(
-    <div>Login Page</div>
+<div>
+    <h1>Login Page</h1>
+    <button onClick={gotoRegister}>Register Here</button>
+</div>
 );
 }
 export default Login
