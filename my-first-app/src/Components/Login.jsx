62bd387675f2f69b1d00d3c53c53e876306b2901
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2023-11-08 22:31:15 +0530

    somechanges

--- my-first-app/src/Components/Login.jsx
+++ my-first-app/src/Components/Login.jsx
@@ -3,37 +3,37 @@ import './Styles/Login.css'
 import { useState } from "react";
 function Login() {
     const redirect = useNavigate();
-    const[loginData,setLoginData]=useState({email:"",password:""})
-    const router=useNavigate();
-   
-    function handleChange(event){
-        setLoginData({...loginData, [event.target.name]: event.target.value})
+    const [loginData, setLoginData] = useState({ email: "", password: "" })
+    const router = useNavigate();
+
+    function handleChange(event) {
+        setLoginData({ ...loginData, [event.target.name]: event.target.value })
     }
 
-   async function Login(event){
+    async function Login(event) {
         event.preventDefault();
-        if(loginData.email && loginData.password){
-            if(loginData.password.length > 7){
-                try{
-                     // const response = await axios.post('http://localhost:8000/login', { loginData })
-                    const response={data:{success:true ,message: "Login Sucessful."}}
-                    if(response.data.success){
+        if (loginData.email && loginData.password) {
+            if (loginData.password.length > 7) {
+                try {
+                    // const response = await axios.post('http://localhost:8000/login', { loginData })
+                    const response = { data: { success: true, message: "Login Sucessful." } }
+                    if (response.data.success) {
                         alert(response.data.message)
-                        setLoginData({email:"",password:""})
+                        setLoginData({ email: "", password: "" })
                         router('/')
                     }
                 }
-                catch(error){
+                catch (error) {
                     alert(error.response.data.message)
                 }
-            }else{
+            } else {
                 alert("Password length must be more than 7 digit.")
             }
-        }else{
+        } else {
             alert("all fields are mandatory!")
         }
     }
-    function gotoRegister(){
+    function gotoRegister() {
         redirect('/form')
     }
     return (
@@ -41,9 +41,9 @@ function Login() {
             <form className="myClass" onSubmit={Login}>
                 <h1>Login</h1>
                 <label>Email</label><br />
-                <input className="inputText" type="email" placeholder="enter email"  onChange={handleChange} name='email' value={loginData.email}/><br />
+                <input className="inputText" type="email" placeholder="enter email" onChange={handleChange} name='email' value={loginData.email} /><br />
                 <label>Password</label><br />
-                <input className="inputText" type="password" placeholder="enter password" onChange={handleChange} name='password' value={loginData.password}/><br />
+                <input className="inputText" type="password" placeholder="enter password" onChange={handleChange} name='password' value={loginData.password} /><br />
                 <button onSubmit={Login}>Login</button>
                 <button onClick={gotoRegister}>Register</button>
             </form>
