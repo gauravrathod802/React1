4c8f9143ab64487bed6042aec4562cff7199c6c3
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2023-11-08 22:28:56 +0530

    LoginPage

--- my-first-app/src/Components/Login.jsx
+++ my-first-app/src/Components/Login.jsx
@@ -1,18 +1,53 @@
 import { useNavigate } from "react-router-dom";
-
-function Login(){
-    const redirect=useNavigate();
-
-    function gotoRegister(){
-        redirect('/Register');
-        redirect('/form');
-
+import './Styles/Login.css'
+import { useState } from "react";
+function Login() {
+    const redirect = useNavigate();
+    const[loginData,setLoginData]=useState({email:"",password:""})
+    const router=useNavigate();
+   
+    function handleChange(event){
+        setLoginData({...loginData, [event.target.name]: event.target.value})
     }
-return(
-<div>
-    <h1>Login Page</h1>
-    <button onClick={gotoRegister}>Register Here</button>
-</div>
-);
+
+   async function Login(event){
+        event.preventDefault();
+        if(loginData.email && loginData.password){
+            if(loginData.password.length > 7){
+                try{
+                     // const response = await axios.post('http://localhost:8000/login', { loginData })
+                    const response={data:{success:true ,message: "Login Sucessful."}}
+                    if(response.data.success){
+                        alert(response.data.message)
+                        setLoginData({email:"",password:""})
+                        router('/')
+                    }
+                }
+                catch(error){
+                    alert(error.response.data.message)
+                }
+            }else{
+                alert("Password length must be more than 7 digit.")
+            }
+        }else{
+            alert("all fields are mandatory!")
+        }
+    }
+    function gotoRegister(){
+        redirect('/form')
+    }
+    return (
+        <div className="container">
+            <form className="myClass" onSubmit={Login}>
+                <h1>Login</h1>
+                <label>Email</label><br />
+                <input className="inputText" type="email" placeholder="enter email"  onChange={handleChange} name='email' value={loginData.email}/><br />
+                <label>Password</label><br />
+                <input className="inputText" type="password" placeholder="enter password" onChange={handleChange} name='password' value={loginData.password}/><br />
+                <button onSubmit={Login}>Login</button>
+                <button onClick={gotoRegister}>Register</button>
+            </form>
+        </div>
+    );
 }
-export default Login
+export default Login;
